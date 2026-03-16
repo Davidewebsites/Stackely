bf65bf1d5dc64fee270c9174caa5033fbf510d82
Davidewebsites <chilo.davide@gmail.com>
2026-03-16 17:12:57 +0100

    fix logo path

--- app/frontend/src/components/StackelyLogo.tsx
+++ app/frontend/src/components/StackelyLogo.tsx
@@ -14,7 +14,7 @@ export default function StackelyLogo({ size = 'sm', showText = true }: StackelyL
     return (
       <div className="flex items-center justify-center">
         <img
-          src="/logo.png?v=11"
+          src="/logo.png"
           alt="Stackely logo"
           style={{ width: 160, height: 'auto', minWidth: 160 }}
           className="flex-shrink-0 object-contain block"
@@ -27,7 +27,7 @@ export default function StackelyLogo({ size = 'sm', showText = true }: StackelyL
   return (
     <div className="flex items-center gap-2.5 mt-[0px] mr-[0px] mb-[0px] ml-[0px] pt-[0px] pr-[0px] pb-[0px] pl-[0px] rounded-none text-[16px] font-normal text-[#020817] bg-[#00000000] opacity-100">
       <img
-        src="/favicon.png?v=11"
+        src="/favicon.png"
         alt="Stackely icon"
         width={26}
         height={26}
